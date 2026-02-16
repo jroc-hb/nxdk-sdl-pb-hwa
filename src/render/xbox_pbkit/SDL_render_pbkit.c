0ad0ce3ae2c3100882189c44c01b54efcd61e802
Justy <darkangel_xi@hotmail.com>
2026-02-16 11:12:06 -0400

    reser

--- src/render/xbox_pbkit/SDL_render_pbkit.c
+++ src/render/xbox_pbkit/SDL_render_pbkit.c
@@ -1,6 +1,5 @@
 /*
-  Simple DirectMedia Layer
-  Xbox NXDK + pbkit renderer with debug instrumentation
+  SDL Xbox PBKit Renderer (Instrumented with DbgPrint)
 */
 
 #include "../../SDL_internal.h"
@@ -21,17 +20,22 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 /* ========================================================= */
-/* ===================== DEBUG MACROS ====================== */
+/* ===================== DEBUG SUPPORT ===================== */
 /* ========================================================= */
 
-#define XBOX_PB_DEBUG 1
-
-#if XBOX_PB_DEBUG
-#define PBDBG(fmt, ...) \
-    DbgPrint("[SDL][PBKIT] " fmt "\n", ##__VA_ARGS__)
-#else
-#define PBDBG(fmt, ...)
+#ifndef RENDERDBG
+#define RENDERDBG 1
 #endif
+
+#if RENDERDBG
+#define RDBG(fmt, ...) DbgPrint("[SDL_XBOX_PB] " fmt "\n", __VA_ARGS__)
+#define RDBG0(msg)     DbgPrint("[SDL_XBOX_PB] " msg "\n")
+#else
+#define RDBG(fmt, ...)
+#define RDBG0(msg)
+#endif
+
+/* ========================================================= */
 
 #define PB_MAXRAM 0x03FFAFFF
 #define PB_MAXZ 16777215.f
@@ -47,186 +51,153 @@
 #define NV2A_VERTEX_ATTR_TEXTURE0 9
 #define NV2A_VERTEX_ATTR_TEXTURE1 10
 
-/* ========================================================= */
-/* ===================== (UNCHANGED CODE) ================== */
-/* ========================================================= */
-
-/* --- SNIP: all unchanged helpers remain exactly as-is --- */
-/* MatrixViewport, MatrixOrtho, MatrixMultiply, PixelFormat... */
-/* FastTextureUpdate, etc.                                   */
+static SDL_bool pbkit_initialized = SDL_FALSE;
 
 /* ========================================================= */
-/* ===================== BLEND MODE ======================== */
+/* ================== DATA STRUCTURES ====================== */
 /* ========================================================= */
 
-static void
-SetBlendMode(XBOX_PB_RenderData *data, int blendmode)
+typedef struct
 {
-    if (blendmode != data->cur_blendmode) {
+    SDL_BlendMode cur_blendmode;
+    SDL_Texture *cur_texture;
+    SDL_Rect cur_viewport;
+    SDL_Rect cur_cliprect;
+    Uint32 cur_color_word;
+    float cur_color[4];
+    unsigned int fb_width;
+    unsigned int fb_height;
+    unsigned int fb_color_fmt;
+    unsigned int fb_color_pitch;
+    unsigned int fb_depth_fmt;
+    unsigned int fb_depth_pitch;
+    unsigned int target_width;
+    unsigned int target_height;
+    SDL_Texture *target;
+    SDL_bool vsync;
+    SDL_bool rendering;
+} XBOX_PB_RenderData;
 
-        PBDBG("SetBlendMode: %d -> %d",
-              data->cur_blendmode, blendmode);
+typedef struct
+{
+    unsigned int tex_format;
+    unsigned int surf_format;
+    unsigned int width;
+    unsigned int height;
+    unsigned int size;
+    unsigned int pitch;
+    unsigned int bytespp;
+    unsigned int filter;
+    unsigned int addr;
+    SDL_bool is_aligned;
+    void *data;
+} XBOX_PB_TextureData;
 
-        Uint32 *p = pb_begin();
+/* ========================================================= */
+/* ===================== GPU HELPERS ======================= */
+/* ========================================================= */
 
-        p = pb_push1(p, NV097_SET_BLEND_ENABLE,
-                     (blendmode != SDL_BLENDMODE_NONE));
+static inline void InitGPUState(void)
+{
+    RDBG0("InitGPUState(): Initializing GPU state");
 
-        switch (blendmode) {
-        case SDL_BLENDMODE_BLEND:
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_SFACTOR,
-                NV097_SET_BLEND_FUNC_SFACTOR_V_SRC_ALPHA);
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_DFACTOR,
-                NV097_SET_BLEND_FUNC_DFACTOR_V_ONE_MINUS_SRC_ALPHA);
-            break;
-        case SDL_BLENDMODE_ADD:
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_SFACTOR,
-                NV097_SET_BLEND_FUNC_SFACTOR_V_SRC_ALPHA);
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_DFACTOR,
-                NV097_SET_BLEND_FUNC_DFACTOR_V_ONE);
-            break;
-        case SDL_BLENDMODE_MOD:
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_SFACTOR,
-                NV097_SET_BLEND_FUNC_SFACTOR_V_ZERO);
-            p = pb_push1(p, NV097_SET_BLEND_FUNC_DFACTOR,
-                NV097_SET_BLEND_FUNC_DFACTOR_V_SRC_COLOR);
-            break;
-        }
+    Uint32 *p = pb_begin();
 
-        pb_end(p);
-        data->cur_blendmode = blendmode;
-    }
+    p = pb_push1(p, NV097_SET_LIGHTING_ENABLE, 0);
+    p = pb_push1(p, NV097_SET_DEPTH_TEST_ENABLE, 0);
+    p = pb_push1(p, NV097_SET_STENCIL_TEST_ENABLE, 0);
+    p = pb_push1(p, NV097_SET_ALPHA_TEST_ENABLE, 0);
+    p = pb_push1(p, NV097_SET_CULL_FACE_ENABLE, 0);
+    p = pb_push1(p, NV097_SET_BLEND_ENABLE, 0);
+
+    pb_end(p);
+
+    while (pb_busy());
+
+    RDBG0("InitGPUState(): GPU state initialized");
 }
 
-/* ========================================================= */
-/* ===================== TEXTURE BIND ====================== */
-/* ========================================================= */
-
-static inline void
-SetTexture(XBOX_PB_RenderData *data, SDL_Texture *texture)
-{
-    if (texture != data->cur_texture) {
-
-        PBDBG("SetTexture: %p -> %p",
-              data->cur_texture, texture);
-
-        if (texture) {
-            XBOX_PB_TextureData *xtex =
-                (XBOX_PB_TextureData *) texture->driverdata;
-
-            PBDBG("  tex: %ux%u pitch=%u addr=0x%08X",
-                  xtex->width, xtex->height,
-                  xtex->pitch, xtex->addr);
-
-            Uint32 *p = pb_begin();
-            p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_TX_ENABLE(0), 0x40000000);
-            p = pb_push2(p, NV20_TCL_PRIMITIVE_3D_TX_OFFSET(0),
-                         xtex->addr, xtex->tex_format);
-            p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_TX_NPOT_PITCH(0),
-                         xtex->pitch << 16);
-            p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_TX_NPOT_SIZE(0),
-                         (xtex->width << 16) | xtex->height);
-            p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_TX_FILTER(0),
-                         xtex->filter);
-            pb_end(p);
-        } else {
-            PBDBG("  disabling texture stage 0");
-            Uint32 *p = pb_begin();
-            p = pb_push1(p, NV20_TCL_PRIMITIVE_3D_TX_ENABLE(0),
-                         0x0003FFC0);
-            pb_end(p);
-        }
-
-        data->cur_texture = texture;
-    }
-}
-
-/* ========================================================= */
-/* ===================== VIEWPORT ========================== */
-/* ========================================================= */
-
-static inline void
-SetViewport(XBOX_PB_RenderData *data, const SDL_Rect vrect)
-{
-    if (SDL_memcmp(&data->cur_viewport, &vrect,
-                   sizeof(SDL_Rect)) != 0) {
-
-        PBDBG("SetViewport: x=%d y=%d w=%d h=%d",
-              vrect.x, vrect.y, vrect.w, vrect.h);
-
-        float mview[16], mortho[16], mproj[16];
-        MatrixViewport(mview, vrect.x, -vrect.y,
-                       vrect.w, vrect.h);
-        MatrixOrtho(mortho, vrect.w, vrect.h);
-        MatrixMultiply(mproj, mortho, mview);
-
-        Uint32 *p = pb_begin();
-        p = pb_push_transposed_matrix(
-            p, NV097_SET_PROJECTION_MATRIX, mproj);
-        pb_end(p);
-
-        data->cur_viewport = vrect;
-    }
-}
-
-/* ========================================================= */
-/* ===================== START/END DRAW ==================== */
-/* ========================================================= */
-
-static inline void
-StartDrawing(XBOX_PB_RenderData *data)
+static inline void StartDrawing(XBOX_PB_RenderData *data)
 {
     if (!data->rendering) {
-        PBDBG("StartDrawing");
-        pb_reset();
+        RDBG("StartDrawing(): fb=%ux%u target=%p",
+             data->fb_width, data->fb_height, data->target);
 
+        pb_reset();
         if (!data->target)
             pb_target_back_buffer();
 
-        pb_erase_depth_stencil_buffer(
-            0, 0, data->fb_width, data->fb_height);
+        pb_erase_depth_stencil_buffer(0, 0,
+                                      data->fb_width,
+                                      data->fb_height);
 
         while (pb_busy());
         data->rendering = SDL_TRUE;
+
+        RDBG0("StartDrawing(): Rendering started");
     }
 }
 
-static inline void
-EndDrawing(XBOX_PB_RenderData *data)
+static inline void EndDrawing(XBOX_PB_RenderData *data)
 {
     if (data->rendering) {
-        PBDBG("EndDrawing (flushing pushbuffer)");
+        RDBG0("EndDrawing(): Waiting for GPU");
+
         while (pb_busy());
         while (pb_finished());
+
         data->rendering = SDL_FALSE;
+        RDBG0("EndDrawing(): Rendering finished");
     }
 }
 
 /* ========================================================= */
-/* ===================== CREATE TEXTURE ==================== */
+/* ===================== TEXTURES ========================== */
 /* ========================================================= */
 
 static int
-XBOX_PB_CreateTexture(SDL_Renderer *renderer,
-                      SDL_Texture *texture)
+XBOX_PB_CreateTexture(SDL_Renderer *renderer, SDL_Texture *texture)
 {
-    PBDBG("CreateTexture: %ux%u format=0x%08X access=%d",
-          texture->w, texture->h,
-          texture->format, texture->access);
+    RDBG("CreateTexture(): %ux%u format=0x%08X access=%d",
+         texture->w, texture->h,
+         texture->format, texture->access);
 
-    /* original body unchanged until allocation */
+    XBOX_PB_TextureData *xtex =
+        (XBOX_PB_TextureData *) SDL_calloc(1, sizeof(XBOX_PB_TextureData));
 
-    /* ... */
+    if (!xtex) {
+        RDBG0("CreateTexture(): Allocation FAILED");
+        SDL_OutOfMemory();
+        return -1;
+    }
 
-    PBDBG("  allocated %u bytes @ %p",
-          xtex->size, xtex->data);
+    xtex->width  = texture->w;
+    xtex->height = texture->h;
+    xtex->bytespp = SDL_BYTESPERPIXEL(texture->format);
+    xtex->pitch = xtex->bytespp * xtex->width;
+    xtex->size  = xtex->pitch * xtex->height;
+    xtex->is_aligned = (xtex->size & 63) == 0;
+
+    xtex->data = MmAllocateContiguousMemoryEx(
+        xtex->size, 0, PB_MAXRAM, 16, 0x404);
+
+    if (!xtex->data) {
+        SDL_free(xtex);
+        SDL_OutOfMemory();
+        return -1;
+    }
+
+    SDL_memset(xtex->data, 0, xtex->size);
+
+    xtex->addr = ((unsigned int)xtex->data) & 0x03FFFFFF;
+
+    texture->driverdata = xtex;
+
+    RDBG("CreateTexture(): Allocated %u bytes at %p pitch=%u",
+         xtex->size, xtex->data, xtex->pitch);
 
     return 0;
 }
-
-/* ========================================================= */
-/* ===================== UPDATE TEXTURE ==================== */
-/* ========================================================= */
 
 static int
 XBOX_PB_UpdateTexture(SDL_Renderer *renderer,
@@ -238,32 +209,43 @@ XBOX_PB_UpdateTexture(SDL_Renderer *renderer,
     XBOX_PB_TextureData *xtex =
         (XBOX_PB_TextureData *) texture->driverdata;
 
-    PBDBG("UpdateTexture: rect=%dx%d @(%d,%d) pitch=%d",
-          rect->w, rect->h,
-          rect->x, rect->y, pitch);
+    RDBG("UpdateTexture(): rect=%dx%d (%d,%d)",
+         rect->w, rect->h, rect->x, rect->y);
 
-    /* original body unchanged */
+    const Uint8 *src = pixels;
+    Uint8 *dst = (Uint8 *)xtex->data +
+                 rect->y * xtex->pitch +
+                 rect->x * xtex->bytespp;
+
+    size_t length = rect->w * xtex->bytespp;
+
+    for (int row = 0; row < rect->h; row++) {
+        SDL_memcpy(dst, src, length);
+        src += pitch;
+        dst += xtex->pitch;
+    }
 
     return 0;
 }
 
-/* ========================================================= */
-/* ===================== SET RENDER TARGET ================= */
-/* ========================================================= */
-
-static int
-XBOX_PB_SetRenderTarget(SDL_Renderer *renderer,
-                        SDL_Texture *texture)
+static void
+XBOX_PB_DestroyTexture(SDL_Renderer *renderer, SDL_Texture *texture)
 {
-    PBDBG("SetRenderTarget: %p", texture);
+    RDBG("DestroyTexture(): %p", texture);
 
-    /* original body unchanged */
+    XBOX_PB_TextureData *xtex =
+        (XBOX_PB_TextureData *) texture->driverdata;
 
-    PBDBG("  target size: %ux%u",
-          xdata->target_width,
-          xdata->target_height);
+    if (!xtex)
+        return;
 
-    return 0;
+    if (xtex->data)
+        MmFreeContiguousMemory(xtex->data);
+
+    SDL_free(xtex);
+    texture->driverdata = NULL;
+
+    RDBG0("DestroyTexture(): Freed texture");
 }
 
 /* ========================================================= */
@@ -279,40 +261,46 @@ XBOX_PB_RunCommandQueue(SDL_Renderer *renderer,
     XBOX_PB_RenderData *data =
         (XBOX_PB_RenderData *) renderer->driverdata;
 
-    PBDBG("RunCommandQueue begin");
+    RDBG0("RunCommandQueue(): Begin");
 
     StartDrawing(data);
 
-    while (cmd) {
+    int cmd_index = 0;
 
-        PBDBG(" Command type: %d", cmd->command);
+    while (cmd) {
+        RDBG(" Command #%d type=%d",
+             cmd_index++, cmd->command);
 
         switch (cmd->command) {
 
-        case SDL_RENDERCMD_CLEAR:
-            PBDBG("  CLEAR");
-            break;
+        case SDL_RENDERCMD_CLEAR: {
+            Uint32 color =
+                (cmd->data.color.a << 24) |
+                (cmd->data.color.r << 16) |
+                (cmd->data.color.g << 8)  |
+                 cmd->data.color.b;
 
-        case SDL_RENDERCMD_DRAW_POINTS:
-            PBDBG("  DRAW_POINTS count=%d",
-                  cmd->data.draw.count);
-            break;
+            RDBG("  CLEAR: color=0x%08X", color);
 
-        case SDL_RENDERCMD_DRAW_LINES:
-            PBDBG("  DRAW_LINES count=%d",
-                  cmd->data.draw.count);
+            pb_fill(0, 0,
+                    data->target_width,
+                    data->target_height,
+                    color);
             break;
+        }
 
-        case SDL_RENDERCMD_FILL_RECTS:
-            PBDBG("  FILL_RECTS count=%d",
-                  cmd->data.draw.count);
+        case SDL_RENDERCMD_SETDRAWCOLOR:
+            RDBG("  SETDRAWCOLOR: (%u,%u,%u,%u)",
+                 cmd->data.color.r,
+                 cmd->data.color.g,
+                 cmd->data.color.b,
+                 cmd->data.color.a);
             break;
 
         case SDL_RENDERCMD_COPY:
         case SDL_RENDERCMD_COPY_EX:
-            PBDBG("  COPY texture=%p count=%d",
-                  cmd->data.draw.texture,
-                  cmd->data.draw.count);
+            RDBG("  COPY: texture=%p",
+                 cmd->data.draw.texture);
             break;
 
         default:
@@ -322,7 +310,8 @@ XBOX_PB_RunCommandQueue(SDL_Renderer *renderer,
         cmd = cmd->next;
     }
 
-    PBDBG("RunCommandQueue end");
+    RDBG0("RunCommandQueue(): End");
+
     return 0;
 }
 
@@ -336,65 +325,111 @@ XBOX_PB_RenderPresent(SDL_Renderer *renderer)
     XBOX_PB_RenderData *data =
         (XBOX_PB_RenderData *) renderer->driverdata;
 
-    PBDBG("RenderPresent (vsync=%d)", data->vsync);
+    RDBG("RenderPresent(): vsync=%d", data->vsync);
 
     EndDrawing(data);
 
-    if (data->vsync)
+    if (data->vsync) {
+        RDBG0("RenderPresent(): Waiting for VBL");
         pb_wait_for_vbl();
+    }
 }
 
 /* ========================================================= */
-/* ===================== DESTROY TEXTURE =================== */
+/* ===================== RENDERER ========================== */
 /* ========================================================= */
 
 static void
-XBOX_PB_DestroyTexture(SDL_Renderer *renderer,
-                       SDL_Texture *texture)
+XBOX_PB_DestroyRenderer(SDL_Renderer *renderer)
 {
-    PBDBG("DestroyTexture: %p", texture);
+    RDBG0("DestroyRenderer()");
 
-    /* original body unchanged */
+    XBOX_PB_RenderData *data =
+        (XBOX_PB_RenderData *) renderer->driverdata;
+
+    if (data) {
+        EndDrawing(data);
+        SDL_free(data);
+    }
+
+    SDL_free(renderer);
 }
 
-/* ========================================================= */
-/* ===================== CREATE RENDERER =================== */
-/* ========================================================= */
-
 SDL_Renderer *
-XBOX_PB_CreateRenderer(SDL_Window *window,
-                       Uint32 flags)
+XBOX_PB_CreateRenderer(SDL_Window * window, Uint32 flags)
 {
-    PBDBG("CreateRenderer flags=0x%08X", flags);
+    RDBG("CreateRenderer(): flags=0x%08X", flags);
 
     if (!pbkit_initialized) {
-        PBDBG("Initializing pbkit...");
         int err = pb_init();
         if (err != 0) {
-            PBDBG("pb_init FAILED: %d", err);
             SDL_SetError("pb_init() returned %d", err);
             return NULL;
         }
 
-        PBDBG("pbkit initialized");
+        RDBG0("pb_init(): SUCCESS");
+
         InitGPUState();
         pbkit_initialized = SDL_TRUE;
     }
 
-    /* original allocation code unchanged */
+    SDL_Renderer *renderer =
+        (SDL_Renderer *) SDL_calloc(1, sizeof(*renderer));
 
-    PBDBG("Backbuffer: %ux%u pitch=%u",
-          data->fb_width,
-          data->fb_height,
-          data->fb_color_pitch);
+    if (!renderer)
+        return NULL;
+
+    XBOX_PB_RenderData *data =
+        (XBOX_PB_RenderData *) SDL_calloc(1, sizeof(*data));
+
+    if (!data) {
+        SDL_free(renderer);
+        return NULL;
+    }
+
+    renderer->driverdata = data;
+    renderer->window = window;
+    renderer->RunCommandQueue = XBOX_PB_RunCommandQueue;
+    renderer->RenderPresent = XBOX_PB_RenderPresent;
+    renderer->DestroyRenderer = XBOX_PB_DestroyRenderer;
+    renderer->CreateTexture = XBOX_PB_CreateTexture;
+    renderer->UpdateTexture = XBOX_PB_UpdateTexture;
+    renderer->DestroyTexture = XBOX_PB_DestroyTexture;
+
+    data->fb_width  = pb_back_buffer_width();
+    data->fb_height = pb_back_buffer_height();
+    data->fb_color_pitch = pb_back_buffer_pitch();
+
+    data->target_width  = data->fb_width;
+    data->target_height = data->fb_height;
+
+    if (flags & SDL_RENDERER_PRESENTVSYNC)
+        data->vsync = SDL_TRUE;
+
+    RDBG("Backbuffer: %ux%u pitch=%u",
+         data->fb_width,
+         data->fb_height,
+         data->fb_color_pitch);
 
     pb_show_front_screen();
 
-    PBDBG("Renderer ready");
+    RDBG0("CreateRenderer(): READY");
 
     return renderer;
 }
 
-/* ========================================================= */
+SDL_RenderDriver XBOX_PB_RenderDriver = {
+    .CreateRenderer = XBOX_PB_CreateRenderer,
+    .info = {
+        .name = "xbox_pbkit",
+        .flags = SDL_RENDERER_ACCELERATED |
+                 SDL_RENDERER_PRESENTVSYNC |
+                 SDL_RENDERER_TARGETTEXTURE,
+        .num_texture_formats = 1,
+        .texture_formats = { SDL_PIXELFORMAT_ARGB8888 },
+        .max_texture_width = 2048,
+        .max_texture_height = 2048,
+     }
+};
 
 #endif
